ff2071a82be0b8f82232ea945d0592a21a3e070a
Igor Bochkor <igorbochkor@gmail.com>
2020-12-03 16:54:52 +0200

    added solution

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.UserNotFoundException;
+
 public class UserService {
 
     public int getUserScore(String[] records, String email) {
-        return 0;
+
+        for (int i = 0; i < records.length; i++) {
+            String[] emailUsScore = records[i].split(":");
+            if (emailUsScore[0].equals(email)) {
+                return Integer.parseInt(emailUsScore[1]);
+            }
+        }
+        throw new UserNotFoundException("User with given email doesn't exist");
     }
 }
